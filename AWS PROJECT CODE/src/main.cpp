19ce7900e9fc584afc2f5ff25b73dacfbd4b4e58
meyers <myanh1738@gmail.com>
2025-12-19 18:30:37 +0300

    Add lock file for schematic with hostname and username information

--- AWS PROJECT CODE/src/main.cpp
+++ AWS PROJECT CODE/src/main.cpp
@@ -2,35 +2,59 @@
 #include "WIFI_CONNECTION.h"
 #include "DHT22.h"
 #include "AIR_PRESSURE.h"
+#include "LIGHT_SENSOR.h"
+#include "SOILMOISTURE.h"
+#include "RTC.h"
 
 WIFI_CONNECTION wifi_connection("SAM25", "Sam@2025");
 DHTSensor dhtsensor;
 AirPressure airpressure;
+LightSensor lightsensor;
+Soilmoisture soilmoisture;
+Rtc rtc1;
+
+// Uganda/East Africa Time (EAT) Configuration
+const char* ntpServer = "africa.pool.ntp.org";
+const long  gmtOffset_sec = 10800;      // UTC +3 hours
+const int   daylightOffset_sec = 0;    // No DST in Uganda
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   wifi_connection.connect();
     if(wifi_connection.isConnected()) {
-    Serial.println("Connected to WiFi SSID: " + String(wifi_connection.getSSID().c_str()));
+    Serial.println(String("Connected to WiFi SSID: ") + String(wifi_connection.getSSID().c_str()));
   } else {
     Serial.println("Not connected to WiFi.");
   }
  
-  airpressure.sensor_setup();
+  lightsensor.setupSensor();
+  soilmoisture.setupSensor();
+  rtc1.setupRTC();
+   if(rtc1.syncWithNTP(ntpServer, gmtOffset_sec, daylightOffset_sec)) {
+        Serial.println("Time synced for Uganda successfully.");
+    } else {
+        Serial.println("Time sync failed.");
+    }
 }
 
 void loop() {
   
   // put your main code here, to run repeatedly:
-  Serial.print("Temperature: ");
-  Serial.print(airpressure.readTemperature());
-  Serial.print("Pressure: ");
-  Serial.print(airpressure.readPressure());
-  Serial.print("Humidity: ");
-  Serial.println(airpressure.readHumidity());
-  Serial.println("altitude: ");
-  Serial.println(airpressure.readAltitude(1013.25));
-  delay(2000);
+  Serial.print("Light Level voltage: ");
+  Serial.println(lightsensor.readLightLevel());
+  Serial.print("V");
+
+  Serial.print("Soil Moisture voltage: ");
+  Serial.println(soilmoisture.readSoilMoisture());
+  Serial.print("V");
+
+  Serial.print("Soil Moisture digital value: ");
+  Serial.println(soilmoisture.digitizeSoilMoisture());
+
+  rtc1.printDateTime();
+  delay(1000);
 }
 
 
