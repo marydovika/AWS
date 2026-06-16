10d6f32dd084db9fcd0512a48197891798a03841
blaisedavis21 <blaisedavis207@gmail.com>
2026-06-16 12:22:58 +0300

    ErrorLogger: add error type defines for RTC, BME280, DHT22, DS18B20, power monitor

--- PROJECT CODE/include/ERROR_LOGGER.h
+++ PROJECT CODE/include/ERROR_LOGGER.h
@@ -22,3 +22,18 @@
 #define COMP_DAVIS            "DAVIS_RAIN"
 #define COMP_WINDSPEED        "WINDSPEED"
 #define COMP_MAIN             "MAIN_LOOP"
+
+#define ERR_RTC_NOT_FOUND         "I2C_NOT_FOUND"
+#define ERR_RTC_POWER_LOSS        "POWER_LOSS"
+#define ERR_RTC_NTP_SYNC_FAIL     "NTP_SYNC_FAIL"
+
+#define ERR_BME_NOT_FOUND         "I2C_NOT_FOUND"
+#define ERR_BME_NAN_READ          "NAN_READ"
+
+#define ERR_DHT_TEMP_FAIL         "TEMP_READ_FAIL"
+#define ERR_DHT_HUM_FAIL          "HUM_READ_FAIL"
+
+#define ERR_DS18B20_DISCONNECTED  "SENSOR_DISCONNECTED"
+
+#define ERR_PWR_I2C_INCOMPLETE    "I2C_READ_INCOMPLETE"
+#define ERR_PWR_READ_FAIL         "READ_FAIL"
